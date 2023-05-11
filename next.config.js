860ceacf304bd1891a1234666112c3cd41319c47
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-05-12 00:48:16 +0500

    FEAT: Set up code base with tailwindcss.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    CONTENTFUL_SPACE_ID: "zqwab88shp19",
+    CONTENTFUL_ACCESS_TOKEN:
+      "I0MIN90DLAJRfk_48_WZ6kXOUzQ-hWQpJM-sj9K0RiE",
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN:
+      "MoE1rjhPMy6fH5P010yhLEVkNNQEVsb8RMkD4rV1ENI",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
