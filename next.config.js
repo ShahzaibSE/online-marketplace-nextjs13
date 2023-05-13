1eed1e452ef2eea3deadd4da2a6fee5e8db8bd94
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-05-13 17:39:20 +0500

    FEAT: Connected vercel serverless postgre database

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
       "I0MIN90DLAJRfk_48_WZ6kXOUzQ-hWQpJM-sj9K0RiE",
     CONTENTFUL_PREVIEW_ACCESS_TOKEN:
       "MoE1rjhPMy6fH5P010yhLEVkNNQEVsb8RMkD4rV1ENI",
+    psql: "postgres://default:Zmb7SkWxwI0g@ep-icy-hill-346142.us-east-1.postgres.vercel-storage.com:5432/verceldb"
   },
 };
 
 module.exports = nextConfig;
+
+// psql "postgres://default:Zmb7SkWxwI0g@ep-icy-hill-346142.us-east-1.postgres.vercel-storage.com:5432/verceldb"
